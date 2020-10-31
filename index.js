956a5fe91aebf7a525955b286be05b87962da920
om0004 <60003596+om0004@users.noreply.github.com>
2020-10-31 18:33:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 let b='';
 let x=0;
+function fun(w)
+{
+	console.log(w);
+	if(w=='light')
+	{
+		document.head.children[4].href="cal_css.css";
+		document.getElementById("mode").style.color="black";
+		document.getElementById("mode").style.backgroundColor="white";
+	}
+	else if(w=='dark')
+	{
+		document.head.children[4].href="cal2_css.css";
+		document.getElementById("mode").style.color="white";
+		document.getElementById("mode").style.backgroundColor="black";
+	}
+}
 function num(a){
 	if(a=='='){
 		console.log("b="+b);
 		var ans=calculate(b);
 		b=ans;
-		console.log(b);
 	}
 	else if(a=='clr'){
 		document.getElementById('output-value').innerHTML='0';
 		b='';
 		a='';
-		console.clear();
 	}
 	else if(a=="bsp"){
 		if(b.length==1){
 			document.getElementById('output-value').innerHTML='0';
-			console.log(0);
 			b=0;
 		}
 		else{
@@ -24,40 +37,33 @@ function num(a){
 			console.log(typeof(b));
 			b=b.slice(0,b.length-1);
 			document.getElementById('output-value').innerHTML=b;
-			console.log(b);
 		}
-		console.log(a);
 	}
 	else{
 	b=b+a;
 	document.getElementById('output-value').innerHTML=b;
-	console.log(a);	
     }
 }
 function calculate(b){
 	try{
 		var i=0;
-		while(1)
-		{
+		while(1){
 			if(b.charAt(i)>'0'||b.charAt(i)<'0')
 			{
 				break;
 			}
 			i++;
 		}
-		if(i>=b.length)
-		{
+		if(i>=b.length){
 			b=0;
 		}
 		else{
 			b=b.slice(i);
 		}
-		
 		x=eval(b);
 		console.log(x);
 		document.getElementById('history-value').innerHTML=b;
 		document.getElementById('output-value').innerHTML=x;
-
 		return x;
        }
        catch(err){
